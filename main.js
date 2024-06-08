7708c59e98de79fffe1b05ab15535663e54a0b7f
Katalin Molnar <molnarlin@gmail.com>
2024-06-08 16:35:51 +0100

    setTimeout function added

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ function setData (){
 
 xhr.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-        setData(JSON.parse(this.responseText));
+        data = JSON.parse(this.responseText);
     }
 };
+
+setTimeout(function(){
+    console.log(data);
+}, 500);
